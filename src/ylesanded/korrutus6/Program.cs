0d9f1ddf9f924ea1bba7ab8ea28f3ee2f51e9ecb
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 14:03:30 +0200

    2

---
+++ src/ylesanded/korrutus6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korrutus6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n < 7; n++)
+            {
+                Console.WriteLine($"{n * 1} | {n * 2} | {n * 3} | {n*4} | {n*5} | {n*6}");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
