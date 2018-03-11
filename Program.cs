063ae8394aa00da7bf3120de18c79d2315cbeda9
SilviVinkman <32923802+SilviVinkman@users.noreply.github.com>
2018-03-11 12:29:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vabandused
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vabandused = new string [4] { "Ei taha", "Ei saa", "Ei oska", "Ei viitsi" };
+
+                Console.WriteLine("Miks kodutöö tegemata?");
+                
+                Console.WriteLine("Töö tegemata, sest " + vabandused[2]); 
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
