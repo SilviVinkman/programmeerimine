01023c166e045ec66c2a587398acf6e10dbcb15a
SilviVinkman <32923802+SilviVinkman@users.noreply.github.com>
2018-03-11 10:09:01 +0200

    Add files via upload

--- G-H_desk_katse2/G-H_desk_katse2/Program.cs
+++ G-H_desk_katse2/G-H_desk_katse2/Program.cs
@@ -10,6 +10,8 @@ namespace G_H_desk_katse2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
         }
     }
 }
