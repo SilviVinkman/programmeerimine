c5d3bee0b646522757b7fff9c3a9cd9549eab1a1
SilviVinkman <32923802+SilviVinkman@users.noreply.github.com>
2018-03-11 13:53:55 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vabandused
+namespace Kordused
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var vabandused = new string [4] { "Ei taha", "Ei saa", "Ei oska", "Ei viitsi" };
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
 
-                Console.WriteLine("Miks kodutöö tegemata?");
-                
-                Console.WriteLine("Töö tegemata, sest " + vabandused[2]); 
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+
 
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
