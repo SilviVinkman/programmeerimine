dbc1de9e08d9d9689bfcd8acfc79ced9af14ebdf
SilviVinkman <32923802+SilviVinkman@users.noreply.github.com>
2018-03-31 11:53:21 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vabandused
+namespace ylesanne2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var vabandused = new string [4] { "Ei taha", "Ei saa", "Ei oska", "Ei viitsi" };
+            var tegelased = new string[10] { "Kaur", "Mattias", "Kristel", "Heleri", "Trevor", "Kristjan", "Kelli", "Kevin", "Marika", "Laura" };
+            Console.WriteLine("Sul on sellised tegelased: "+ tegelased[0]+" " + tegelased[1] + " " + tegelased[2] + " " + tegelased[3] + " " + tegelased[4] + " " + tegelased[5] + " " + tegelased[6] + " " + tegelased[7] + " " + tegelased[8] + " " + tegelased[9]);
+            Console.WriteLine("Sisesta mõned laused, kus keegi tegelastest midagi teeb. Ma kirjutan kõik nimed suurte algustähtedega");
+            var input = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(input))
+            { Console.WriteLine("Viga sisestamisel"); }
+            else
+            { Console.WriteLine("Sa sisestasid: " + input); }// kontrolliks
 
-                Console.WriteLine("Miks kodutöö tegemata?");
-                
-                Console.WriteLine("Töö tegemata, sest " + vabandused[2]); 
+            var sõnadlauses = input.Split(new char[] { ' ', '.' }); // saan sisestatud sõnade array?
 
-            Console.WriteLine("Press any key to continue");
+            foreach
+             //tahan sisestatud sõnade loetelust leida üles sellised sõnad=nimed, 
+             //mis on olemas algses nimede loetelus massiiv
+             (var nimilauses in tegelased)
+            //annan nendele nimedele suure algustähe: muudetakse täht indeksiga 0, so esimene täht.
+            //teised tähed nimes väikestena 
+            {
+                var nimiSuurTähega = char.ToUpper(nimilauses[0]) + nimilauses.ToLower().Substring(1);
+
+                // tahan saada tulemust: nimed suure algustähega
+            }
+            Console.WriteLine(String.Join(" ",sõnadlauses));
+            Console.WriteLine("Press any key");
             Console.ReadLine();
         }
     }
